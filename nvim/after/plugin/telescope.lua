af3b06161a73f0127fd55bd95cd87bb351f5e9e8
JavaIsNotHard <jibeshshrestha00@gmail.com>
2022-12-22 13:57:00 +0545

    nvim stuff

---
+++ nvim/after/plugin/telescope.lua
@@ -0,0 +1,27 @@
+require("telescope").setup{
+  defaults = {
+    mappings = {
+      n = {
+    	  ['<c-d>'] = require('telescope.actions').delete_buffer
+      }, -- n
+      i = {
+        ['<c-d>'] = require('telescope.actions').delete_buffer
+      } -- i
+    },
+ },
+ extensions = {
+    file_browser = {
+      theme = "ivy",
+      hijack_netrw = true,
+    },
+  },
+}
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fp', function()
+	builtin.grep_string({search = vim.fn.input("Grep >") });
+end)
